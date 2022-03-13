7c30f3802803786c0f38f48c42574be523db34c6
Kristin Hill <krstinhill4211@gmail.com>
2022-03-12 16:35:25 -0800

    initial commit

---
+++ Assets/tilesets/swamp_background_objects.tsx
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="swamp_background_objects" tilewidth="199" tileheight="183" tilecount="53" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="101" height="103" source="free-swamp-game-tileset-pixel-art/3 Objects/Trees/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="104" height="132" source="free-swamp-game-tileset-pixel-art/3 Objects/Trees/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="123" height="150" source="free-swamp-game-tileset-pixel-art/3 Objects/Trees/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="104" height="135" source="free-swamp-game-tileset-pixel-art/3 Objects/Willows/1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="141" height="168" source="free-swamp-game-tileset-pixel-art/3 Objects/Willows/2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="199" height="183" source="free-swamp-game-tileset-pixel-art/3 Objects/Willows/3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="31" height="15" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/1.png"/>
+ </tile>
+ <tile id="15">
+  <image width="30" height="10" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="29" height="15" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/3.png"/>
+ </tile>
+ <tile id="17">
+  <image width="31" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/4.png"/>
+ </tile>
+ <tile id="18">
+  <image width="63" height="28" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/5.png"/>
+ </tile>
+ <tile id="19">
+  <image width="31" height="26" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/6.png"/>
+ </tile>
+ <tile id="20">
+  <image width="61" height="24" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/7.png"/>
+ </tile>
+ <tile id="21">
+  <image width="63" height="27" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/8.png"/>
+ </tile>
+ <tile id="22">
+  <image width="61" height="25" source="free-swamp-game-tileset-pixel-art/3 Objects/Bushes/9.png"/>
+ </tile>
+ <tile id="23">
+  <image width="17" height="18" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="17" height="16" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="14" height="16" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="12" height="12" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/4.png"/>
+ </tile>
+ <tile id="27">
+  <image width="11" height="13" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/5.png"/>
+ </tile>
+ <tile id="28">
+  <image width="11" height="9" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/6.png"/>
+ </tile>
+ <tile id="29">
+  <image width="7" height="14" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/7.png"/>
+ </tile>
+ <tile id="30">
+  <image width="8" height="14" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/8.png"/>
+ </tile>
+ <tile id="31">
+  <image width="6" height="8" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/9.png"/>
+ </tile>
+ <tile id="32">
+  <image width="9" height="11" source="free-swamp-game-tileset-pixel-art/3 Objects/Grass/10.png"/>
+ </tile>
+ <tile id="39">
+  <image width="63" height="59" source="free-swamp-game-tileset-pixel-art/3 Objects/Stones/1.png"/>
+ </tile>
+ <tile id="40">
+  <image width="47" height="57" source="free-swamp-game-tileset-pixel-art/3 Objects/Stones/2.png"/>
+ </tile>
+ <tile id="41">
+  <image width="52" height="63" source="free-swamp-game-tileset-pixel-art/3 Objects/Stones/3.png"/>
+ </tile>
+ <tile id="42">
+  <image width="57" height="30" source="free-swamp-game-tileset-pixel-art/3 Objects/Stones/4.png"/>
+ </tile>
+ <tile id="43">
+  <image width="31" height="31" source="free-swamp-game-tileset-pixel-art/3 Objects/Stones/5.png"/>
+ </tile>
+ <tile id="44">
+  <image width="89" height="26" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="40" height="28" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/2.png"/>
+ </tile>
+ <tile id="46">
+  <image width="109" height="41" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/3.png"/>
+ </tile>
+ <tile id="47">
+  <image width="65" height="61" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/4.png"/>
+ </tile>
+ <tile id="48">
+  <image width="81" height="29" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/5.png"/>
+ </tile>
+ <tile id="49">
+  <image width="47" height="55" source="free-swamp-game-tileset-pixel-art/3 Objects/Ridges/6.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="13" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/3.png"/>
+ </tile>
+ <tile id="53">
+  <image width="13" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/4.png"/>
+ </tile>
+ <tile id="54">
+  <image width="13" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/5.png"/>
+ </tile>
+ <tile id="55">
+  <image width="13" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/6.png"/>
+ </tile>
+ <tile id="56">
+  <image width="13" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/7.png"/>
+ </tile>
+ <tile id="57">
+  <image width="11" height="21" source="free-swamp-game-tileset-pixel-art/3 Objects/Pointers/8.png"/>
+ </tile>
+ <tile id="58">
+  <image width="23" height="18" source="free-swamp-game-tileset-pixel-art/3 Objects/Fence/1.png"/>
+ </tile>
+ <tile id="59">
+  <image width="25" height="20" source="free-swamp-game-tileset-pixel-art/3 Objects/Fence/2.png"/>
+ </tile>
+ <tile id="60">
+  <image width="16" height="13" source="free-swamp-game-tileset-pixel-art/3 Objects/Fence/3.png"/>
+ </tile>
+ <tile id="61">
+  <image width="34" height="28" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/1.png"/>
+ </tile>
+ <tile id="62">
+  <image width="34" height="27" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/2.png"/>
+ </tile>
+ <tile id="63">
+  <image width="27" height="26" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/3.png"/>
+ </tile>
+ <tile id="64">
+  <image width="33" height="26" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/4.png"/>
+ </tile>
+ <tile id="65">
+  <image width="34" height="27" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/5.png"/>
+ </tile>
+ <tile id="66">
+  <image width="27" height="26" source="free-swamp-game-tileset-pixel-art/3 Objects/Boxes/6.png"/>
+ </tile>
+</tileset>
